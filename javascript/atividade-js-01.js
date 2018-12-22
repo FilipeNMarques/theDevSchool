3013d62371ca65e736372e00c5d86c1b782cc069
italo gabriel <italogabriel2804@gmail.com>
2018-12-22 18:42:04 -0300

    Adicionado arquivos da aula 3 de JS e atividade 01

---
+++ javascript/atividade-js-01.js
@@ -0,0 +1,7 @@
+noel = "Papai Noel"
+peru = "Peru de Natal"
+neve = "Neve"
+rena = "Renas"
+
+numNoel = 1
+numPeru = 1
--- javascript/script.js
+++ javascript/script.js
@@ -1,3 +1,38 @@
-name = "Ítalo Gabriel";
+name = "Ítalo Gabriel"; //String
+age = 17; //Int
+human = true; //Boolean  
+height = 1.70; //Float
 
-document.write(name);
+//Comando para escrever no html
+//document.write(name); 
+
+//Comando para criar uma caixa de dialogo no html
+//alert() 
+
+welcome = "Seja bem vindo";
+
+//alert (welcome + " " + name);
+
+info = "Anos de idade";
+tem = "tem";
+//document.write(name + " " + tem + " " + age + " " + info); //Exemplo de concatenação (Juntar uma variavel com outras Strings no meio)
+
+uva = 100;
+pera = 10;
+limao = 3;
+morango = 5;
+
+//Exemplo de operadores matematicos simples no Js
+//document.write(uva / limao);
+//document.write(pera + uva);
+//document.write(morango - limao);
+//document.write(morango * uva);
+
+document.getElementById('btn')
+
+//Atividade para fixar conteudo variaveis 
+//1- Fazer 4 variaveis com Strings, 4 variaveis com Int e 4 variaveis com Floats com o tema Natal
+//2- Concatenar as 4 variaveis Int com as Strings (ex: Italo + Idade) saida Italo tem 17 anos
+//3- Imprimir os resultados das variaveis e das concatenações no index.html em divs separadas
+//4- Criar um arquivo CSS para fazer os estilos das divs *Bonus = Fazer um estilo legal para pagina inteira*
+
