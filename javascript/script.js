d6eefdb3f05cf3edd2558db3ec5f9f503c9b222d
Filipe Marques <filipi.marques@gmail.com>
2018-12-27 13:56:24 -0300

    Add js files

--- javascript/script.js
+++ javascript/script.js
@@ -1,7 +1,37 @@
 //Variavel
 
-name = "Filipe Marques";
-
+name = "Filipe Marques"; //String
+age = 27; //Int
+human = true; //Boolean
+height = 1.83; // Float
 //String são representadas por palavras dentro de aspas, ex: "Rasgador de pipas"
+//Comando para escrever no html
+// document.write(name);
+//Comando para criar uma caixa de diálogo no html
+// alert(height);
 
-document.write(name);
+welcome = "Seja bem vindo";
+
+//alert(welcome + " " + name); // Exemplo de concatenação (Juntar uma variável com outras e strings no meio
+
+uva = 100;
+pera = 10;
+limao = 3;
+morango = 5;
+
+// Exemplo de operadores matematicos simples no js
+//document.write(uva / limao);
+//document.write(pera + uva);
+//document.write(morango - limao);
+//document.write(morango * uva);
+
+//Atividade para fixar conteúdo de variáveis
+
+// 2. Fazer 4 variãveis com strings, 4 variaveis com ints e 4 variaveis com floats com o tema natal
+// 3. Concatenas as 4 variaveis int com as strings (ex: Italo + idade) saída Italo tem 17 anos
+// 4. Fazer o link do arquivo atividade-js-01.js com index.html da pasta atual.
+// 5. Imprimir os resultados das variaveis e das concatenações no index.html em divs separadas.
+// 6. Cirar um arquivo CSS para fazer os estilos das divs *BONUS = fazer um estilo legal para página inteira*
+
+uva, pera, limao;
+var frutas = document.getElementById(variaveis);
